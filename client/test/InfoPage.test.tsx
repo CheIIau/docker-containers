fc3acfd965b7b50d600aee66358fb987319dd292
v.prividenko <rip.rulez321@gmail.com>
2023-05-19 00:41:33 +0300

    fix: fix test

--- client/test/InfoPage.test.tsx
+++ client/test/InfoPage.test.tsx
@@ -2,10 +2,11 @@ import { render } from '@solidjs/testing-library'
 import InfoPage from '../src/pages/InfoPage'
 import { describe, expect, it } from 'vitest'
 import '@testing-library/jest-dom'
+import { Router } from '@solidjs/router'
 
 describe('InfoPage', () => {
   it('renders the page', () => {
-    const { getByText } = render(() => <InfoPage />)
+    const { getByText } = render(() => <Router><InfoPage /></Router>)
     expect(getByText('Some info')).toBeInTheDocument()
   })
 })
